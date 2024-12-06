cc24b1b26cb1224f80bd50e7dbd5be0dcb752334
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-12-06 15:07:22 +0600

    express response object

--- express.js/index.js
+++ express.js/index.js
@@ -1,106 +1,46 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 const app = express()
-const admin = express()
+const admin = express.Router()
+// app.use(express.json())
+app.use(express.text())
+app.use(cookieParser())
 
-app.set('view engine', 'ejs')
-
-// app.locals is local variables within the application. which we can use entire application.
-app.locals.title = "My app"
-app.locals.port = 3000
-
-// routing
-// app.get('/', (req,res)=>{
-//     console.log(app.mountpath)      // return the path where this app is mount
-//     res.send("This is Public dashboard")
-// })
-
-//  call parent of parant app
-// admin.on('mount',(parent)=>{
-//     console.log('Admin Mounted')
-//     console.log(parent) // refers to the parent app
-//   })
-
-// app.use('/admin', admin)
-// admin.get('/',(req,res)=>{
-//     console.log(admin.mountpath)
-
-//     res.send("This is admin dashboard")
-// })
-  
-// Application methods
-// app.all()
-app.all('/',(req, res)=>{
-    res.send('welcome to application home')
+admin.get('/dashboard/:id', (req,res)=>{
+    console.log('RUL:',req.url)
+    console.log('baseURL:',req.baseUrl)
+    console.log('originalURL:',req.originalUrl)
+    console.log('path:',req.path)
+    console.log('hostname:',req.hostname)
+    console.log('IP:',req.ip)
+    console.log('method:',req.method)
+    console.log('protocol:',req.protocol)
+    console.log('parameter:',req.params)
+    console.log('query:',req.query)
+    console.log('cookies:',req.cookies)
+    console.log('secure:',req.secure)
+    console.log('route:',req.route)
+    res.send("we are in admin dashboard")
 })
 
-// app.disable and app.enable .parameter would choose from express Application Settings
-app.enable('case sensitive routing')
-app.disable('env')
+app.use('/admin', admin)
 
-// there have 2type of use of app.get(). one respons get method asuseal. another one is to get setting which set by set method
-app.set('title', 'public dashboard')
-app.get('title') 
-
-// app.param()
-app.get('/user/:id',(req,res)=>{
-    console.log(req.userDetails)
-    res.send('Welcome to application user')
+app.get('/',(req,res)=>{
+    console.log(req.url)
+    console.log(req.baseUrl)
+    console.log(req.originalUrl)
+    console.log(req.path)
+    res.send("Hellow, We are starting request object")
 })
 
-app.param('id', (req, res, next, id)=>{
-    const user = {
-        uID: id,
-        name: 'Rafi Sharkar'
-    }
-    req.userDetails = user
-    next()
-})      // this is middleware. it happened before get app gose to callBack
-
-
-// app.path()
-const blog = express()
-const blogAdmin = express()
-
-app.use('/blog', blog)
-blog.use('/admin', blogAdmin)
-
-console.dir(app.path())         // return => // ''
-console.dir(blog.path())        // return => // '/blog'
-console.dir(blogAdmin.path())   // return => // '/admin'
-
-// app.route()
-    /*
-app.get('/rafi/sharkar/777',(req,res)=>{
-    res.send('welcome to application home get')
-})
-app.post('/rafi/sharkar/777',(req,res)=>{
-    res.send('welcome to application home post')
-})
-app.put('/rafi/sharkar/777',(req,res)=>{
-    res.send('welcome to application home put')
-})
-    */
-
-// we can write above as
-app.route('/rafi/sharkar/777')
-    .get((req,res)=>{
-        res.render('pages/about')
-    })
-    .post((req,res)=>{
-        res.send('welcome to application home post')
-    })
-    .put((req,res)=>{
-        res.send('welcome to application home put')
-    })
-
-// app.engine
-
-
-
-app.listen(app.locals.port,()=>{
-    console.log(app.locals.title)
-    console.log(`Server start at ${app.locals.port}`)
+app.post('/edit', (req, res)=>{
+    console.log(req.body)
+    console.log('accept:',req.accepts('json'))
+    console.log('content-type:', req.get('content-type'))
+    res.send("this is for edit")
 })
 
-
+app.listen(3000, ()=>{
+    console.log("Server port 3000")
+})
